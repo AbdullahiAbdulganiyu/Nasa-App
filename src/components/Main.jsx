e46ccd6095da9f355bc032ecece690e14d38d434
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-23 16:18:32 +0100

    using it to style the div having the image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 export default function Main() {
   return (
-    <div>
+    <div className="imgContainer">
       <img src="mars.png" alt="mars-demo-picture" className="bgImage" />;
     </div>
   );
