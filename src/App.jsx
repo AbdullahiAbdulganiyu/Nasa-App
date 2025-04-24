87ebef06181e150f33166a20a16116dbe1378985
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-24 22:24:05 +0100

    passing it to the sidebar as a prop

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Main />
-      {showModal && <SideBar />}
+      {showModal && <SideBar handleToggleModal={handleToggleModal} />}
       <Footer handleToggleModal={handleToggleModal} />
     </>
   );
