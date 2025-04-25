0d8483bc494e2e46b220b6435027fbdeb558b233
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-25 20:16:15 +0100

    storing the result in the local storage

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       try {
         const res = await fetch(url);
         const apiData = await res.json();
+        localStorage.setItem("localKey", JSON.stringify(apiData));
         setData(apiData);
         console.log("DATA\n", apiData);
       } catch (err) {
